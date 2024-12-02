5f102b154a09ee17742abf3abfaa72c53ef16d27
24LP2767 24LP2767 <24LP2767@G301-CC0194.local>
2024-12-02 17:58:34 +0800

    math

---
+++ index.js
@@ -0,0 +1,50 @@
+const number = 1;
+const stringNumber = "1";
+
+console.log(number === stringNumber);
+console.log(number == stringNumber);
+
+const graduesa= 30;
+const graduesb= 30;
+const graduesc= 30;
+
+console.log(graduesa * graduesb * graduesc)
+
+
+const some1= 20;
+const some2= 20;
+
+console.log((some1 * some2)/2)
+
+// m = y2-y1/x2-x1
+
+const y = 100;
+const x= 10;
+const result = (y*2-y*1)/(x*2-x*1)
+
+console.log("m = ", result)
+
+const g = 15;
+const g1 = 3;
+
+console.log(g/g1)
+console.log("ur dun", result)
+
+//гурвалжны талбайг//
+
+const gradues1= 15;
+const gradues2= 30;
+const gradues3= 20;
+const resultss = (gradues1 * gradues2 * gradues3)/2
+console.log(resultss)
+
+
+//(0°C × 9/5) + 32 = 32°F//
+
+const c=1 
+const f= 2
+
+console.log((0c * 9/5) + 32)
+
+console.log(result)
+
