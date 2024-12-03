d0b71f3714b7729d35a6bf9a3ff5f6eea9e39834
24LP2767 24LP2767 <24LP2767@G301-CC0194.local>
2024-12-03 15:09:59 +0800

    op

--- index.js
+++ index.js
@@ -4,47 +4,58 @@ const stringNumber = "1";
 console.log(number === stringNumber);
 console.log(number == stringNumber);
 
-const graduesa= 30;
-const graduesb= 30;
-const graduesc= 30;
+const graduesa = 30;
+const graduesb = 30;
+const graduesc = 30;
 
-console.log(graduesa * graduesb * graduesc)
+console.log(graduesa * graduesb * graduesc);
 
+const some1 = 20;
+const some2 = 20;
 
-const some1= 20;
-const some2= 20;
-
-console.log((some1 * some2)/2)
+console.log((some1 * some2) / 2);
 
 // m = y2-y1/x2-x1
 
 const y = 100;
-const x= 10;
-const result = (y*2-y*1)/(x*2-x*1)
+const x = 10;
+const result = (y * 2 - y * 1) / (x * 2 - x * 1);
 
-console.log("m = ", result)
+console.log("m =", result);
 
 const g = 15;
 const g1 = 3;
 
-console.log(g/g1)
-console.log("ur dun", result)
+const result1 = g / g1;
+console.log("ur dun", result1);
 
 //гурвалжны талбайг//
 
-const gradues1= 15;
-const gradues2= 30;
-const gradues3= 20;
-const resultss = (gradues1 * gradues2 * gradues3)/2
-console.log(resultss)
-
+const gradues1 = 15;
+const gradues2 = 30;
+const gradues3 = 20;
+const gurvaljin = (gradues1 * gradues2 * gradues3) / 2;
+console.log("gurvaljin", gurvaljin);
 
 //(0°C × 9/5) + 32 = 32°F//
 
-const c=1 
-const f= 2
+const c = 1;
 
-console.log((0c * 9/5) + 32)
+const Nigga = c * (9 / 5) + 32;
 
-console.log(result)
+console.log("para", Nigga);
 
+// 14. Хоёр багийн дундаж оноо ( A team - 96, 108, 89; B team - 88, 91, 110 )
+
+const a1 = -96; 
+const a2 = 108;
+const a3 = 89;
+
+const b1 = -88; 
+const b2 = 91;
+const b3 = 110;
+
+const dundaj1 = ((a1+a2+a3)/3)
+const dundaj2 = ((b1+b2+b3)/3)
+
+console.log("dundajuud", dundaj1, dundaj2)
